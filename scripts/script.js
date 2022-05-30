10311ff65d55c748faf92a704ecbf5a6041ff2e7
jiahern <jiahern@student.unimelb.edu.au>
2022-05-30 16:20:08 +0800

    added hover effect and restructure file

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,22 @@ const overlay = document.querySelector("[data-overlay]");
 const navOpenButton = document.querySelector("[data-nav-open-btn]");
 const navBar = document.querySelector("[data-navbar]");
 const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-
 const navElements = [overlay, navOpenButton, navCloseBtn];
 
+console.log(navOpenButton);
 for (let i = 0; i < navElements.length; i++) {
   navElements[i].addEventListener("click", function () {
     navBar.classList.toggle("active");
     overlay.classList.toggle("active");
   });
+}
+
+// Filter Section Dynamic
+const href = window.location.href;
+
+// Target href search-result
+const searchResults = "search-result.html";
+
+if (href.includes(searchResults)) {
+  document.querySelector("[data-filter]").style.marginTop = "2rem";
 }
